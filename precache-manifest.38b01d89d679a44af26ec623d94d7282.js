18465a2a66b24c9894fc3dd57198e460476984eb
IsaPirola <belapirola@gmail.com>
2024-08-01 17:27:15 -0300

    Updates

---
+++ precache-manifest.38b01d89d679a44af26ec623d94d7282.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1c8fd83d40b009e85f7eb06f5b2249c",
+    "url": "/pixels-master/index.html"
+  },
+  {
+    "revision": "c19bb2f70646602a1b32",
+    "url": "/pixels-master/static/css/main.8faabb7f.chunk.css"
+  },
+  {
+    "revision": "1b17ef6e2b62aad0a804",
+    "url": "/pixels-master/static/js/2.95bbc5fe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/pixels-master/static/js/2.95bbc5fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c19bb2f70646602a1b32",
+    "url": "/pixels-master/static/js/main.d9b95630.chunk.js"
+  },
+  {
+    "revision": "763e59301cc6f25ffbc6",
+    "url": "/pixels-master/static/js/runtime-main.9a0428db.js"
+  }
+]);
